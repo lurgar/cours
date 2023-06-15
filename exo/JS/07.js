6495c2ac354eb592d9eb27dd9cca6cb779aad4ed
Ahmed <ahmedmorle@outlook.fr>
2023-06-15 16:47:16 +0200

    first

---
+++ exo/JS/07.js
@@ -0,0 +1,11 @@
+var nb1 =parseInt("prompt"("numero1"));
+var nb2 =parseInt("prompt"("numero2"));
+
+if((!isNAN(nb1))) && ((isNaN(nb2))){
+    document.write("")
+}
+
+(nb1>nb2){
+    document.write("votre 1er nombre:"+nb1+"est plus grand que votre 2nd:"+
+    nb2);
+}
---
+++ exo/JS/3_TVA.js
@@ -0,0 +1,3 @@
+var horsTaxe=prompt("prix Ht : ");
+console.log("le prix TTC  est de: "+(horsTaxe*1.2)+"€.");
+document.write("<h3>Résultat <br> le prix TTC est de"+(horsTaxe*1.2)+"€. </h3>");
