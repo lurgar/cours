db927104b9b3660429f29bf573c4801a2e6b9dc8
Ahmed <ahmedmorle@outlook.fr>
2023-06-28 16:56:59 +0200

    first

---
+++ html - css/GRID/js/activ.js
@@ -0,0 +1,5 @@
+// Navigation avec les liens actifs sur la page en cours
+
+// 1- ajouter l'ID #activ_nav sur la balise <nav>
+// 2- vérifier que la classe .activ est bien dans le fichier css
+// 3- créer la fonction setActiv() pour ajouter la classe .activ sur le lien URL en cours
